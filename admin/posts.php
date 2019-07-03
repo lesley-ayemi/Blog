8fc9c0648d9a261f333a47cd82a99fa8c4237874
lesley <leeayemi@gmail.com>
2019-07-03 12:04:16 +0100

    viewing all post from the dashboard

--- admin/posts.php
+++ admin/posts.php
@@ -31,12 +31,12 @@
 
         switch($source){
 
-            case '34';
-            echo "NICE 34";
+            case 'add_post';
+            include "includes/add_post.php";
             break;
 
-            case '100';
-            echo "NICE 100";
+            case 'edit_post';
+            include "includes/edit_post.php";
             break;
 
             case '200';
